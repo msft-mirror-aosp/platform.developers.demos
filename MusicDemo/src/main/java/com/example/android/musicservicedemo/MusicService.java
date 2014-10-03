7236969fa8d6fec5b6741e09549df335f2542e46
Renato Mangini <mangini@google.com>
2014-10-03 16:42:31 -0700

    Support hardware media buttons on Gearhead sample.
    Bug: 17813653
    Change-Id: I094f67931ad02d7cf29ae90a0917c435f302cada

--- MusicDemo/src/main/java/com/example/android/musicservicedemo/MusicService.java
+++ MusicDemo/src/main/java/com/example/android/musicservicedemo/MusicService.java
@@ -181,6 +181,8 @@ public class MusicService extends MediaBrowserService implements OnPreparedListe
         mSession = new MediaSession(this, "MusicService");
         setSessionToken(mSession.getSessionToken());
         mSession.setCallback(new MediaSessionCallback());
+        mSession.setFlags(MediaSession.FLAG_HANDLES_MEDIA_BUTTONS |
+                MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS);
         updatePlaybackState(null);
 
         mMediaNotification = new MediaNotification(this);
